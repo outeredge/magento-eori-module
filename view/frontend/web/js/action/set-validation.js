0ba058d41411c88800ea3fb9bff2b74dea435a7f
Beniamin Sinca <beni.sinca@gmail.com>
2025-02-13 18:47:36 +0000

    Update set-validation.js
    Call the API to verify the EORI number parsed

--- view/frontend/web/js/action/set-validation.js
+++ view/frontend/web/js/action/set-validation.js
@@ -2,7 +2,7 @@ define(["jquery"], function ($) {
     "use strict";
     if (!$('.checkout-index-index').length) return;
 
-    var isEU = true;
+    var isEU = false;
     window.eoriRequired = false;
 
     function waitForElement(selector, callback, checkFrequencyInMs) {
@@ -17,35 +17,49 @@ define(["jquery"], function ($) {
             }, checkFrequencyInMs);
           }
         })();
-      }
+    }
 
     const setEoriNumber = function() {
         waitForElement('[name="shippingAddress.custom_attributes.eori_number"]', function() {
-            document.querySelector('[name="shippingAddress.custom_attributes.eori_number"]').style.display = 'none';
-            document.querySelector('[name="shippingAddress.custom_attributes.eori_number"]').classList.add('_required');
             checkInputs();
         }, 1000);
     }
 
     const checkInputs = function() {
-        const euCountries = ['AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR',
-                             'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
-                             'SI', 'ES', 'SE'];
+        const EU_COUNTRIES = [
+            'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR',
+            'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK',
+            'SI', 'ES', 'SE'
+        ];
 
         waitForElement('[name="country_id"]', function() {
+            // Detect country change and initiate EORI listeners
             $('[name="country_id"]').change(function() {
                 var countryCode = $(this).val();
-                isEU = euCountries.includes(countryCode);
-                toggle();
+                isEU = EU_COUNTRIES.includes(countryCode);
+                toggleElement();
+                isRequired();
             });
 
             var initialCountry = $('[name="country_id"]').val();
-            isEU = euCountries.includes(initialCountry);
-            toggle();
+            isEU = EU_COUNTRIES.includes(initialCountry);
+
+            // init EORI field
+            toggleElement();
+            isRequired();
         });
     };
 
-    const toggle = function() {
+
+    const isRequired = function () {
+        if (isEU == true) {
+            document.querySelector('[name="shippingAddress.custom_attributes.eori_number"]').classList.add('_required');
+        } else {
+            document.querySelector('[name="shippingAddress.custom_attributes.eori_number"]').classList.remove('_required');
+        }
+    }
+
+    const toggleElement = function() {
         if (isEU == true) {
             document.querySelector('[name="shippingAddress.custom_attributes.eori_number"]').style.display = 'block';
             window.eoriRequired = true;
